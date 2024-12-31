395bb16a36353512974519c323def2d873490d3d
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-31 16:28:04 +0530

    fixed; vercel error

--- app/my_components/Drawer.tsx
+++ app/my_components/Drawer.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import * as React from "react";
 import Link from "next/link";
@@ -16,11 +16,8 @@ import {
 } from "@/components/ui/drawer";
 
 export function DrawerDemo() {
-  const [goal, setGoal] = React.useState(350);
+  
 
-  function onClick(adjustment: number) {
-    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
-  }
 
   return (
     <Drawer>
