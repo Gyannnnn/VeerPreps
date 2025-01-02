2d97a15be3c6efec10bedef477106171705539a3
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-01-02 17:43:36 +0530

    Feature; Saved Posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+ 
   return (
-    <div className=''>page</div>
+    <div className='min-h-screen w-screen bg-secondary dark:bg-zinc-950 flex justify-center'>
+      <div className=' w-[90vw] h-screen bg-red-900 mt-16'>
+        <h1 className='text-4xl text-white'>Gyanaranjan Patra</h1>
+
+      </div>
+    </div>
   )
 }
