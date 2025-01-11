d51a5a9491f6ff4ec3748135e3502488065b4574
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-01-11 23:22:55 +0530

    feat; pdf vivewr

--- app/my_components/pages/Viewer.tsx
+++ app/my_components/pages/Viewer.tsx
@@ -1,19 +1,43 @@
 "use client";
-import React from "react";
-import Pdfviewer from "../Pdfviewer";
-import Link from "next/link";
 
-export default function Viewer() {
+
+import { useState } from "react";
+import { Document, Page } from "react-pdf";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+export default function MyApp() {
+  const [numPages, setNumPages] = useState<number>(0);
+
+  // Function to handle when PDF is loaded
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
+
+
   return (
-    <div className="min-h-screen w-screen mt-16 flex items-center justify-center overflow-hidden flex-col relative">
-      <Link
-        className="px-8 py-3 rounded-md bg-blue-500 fixed top-20 right-6 z-10 text-sm text-white"
-        href="https://gyannnnn.github.io/iitkirba.io/db/pyq/branch/it/2y/oop/oop_end_2024.pdf"
-      >
-        Download
-      </Link>
 
-      <Pdfviewer />
+    <div className="min-h-screen w-screen flex justify-center items-center">
+      <div className="mt-16 bg-secondary w-full p-4">
+        <Document
+          className="flex flex-col items-center justify-center gap-2" // Use flex-column to stack pages vertically
+          renderMode="canvas"
+          file="/dld_end_2024.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {Array.from({ length: numPages }, (_, index) => (
+            <Page
+              key={index}
+              width={1000}
+              pageNumber={index + 1} // Page numbers are 1-indexed
+            />
+          ))}
+        </Document>
+      </div>
+
     </div>
   );
 }
