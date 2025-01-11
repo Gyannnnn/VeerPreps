c577d421a1d5a602d7e5d03a7bdea7cfc286e3a4
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-01-11 23:32:57 +0530

    dd

--- app/my_components/pages/Viewer.tsx
+++ app/my_components/pages/Viewer.tsx
@@ -6,6 +6,8 @@ import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
