59fdc4d2b99500a5fcfd5524d427fb2899ff32d7
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-01-11 20:33:01 +0530

    feat; pdf viewer

--- app/my_components/pages/Viewer.tsx
+++ app/my_components/pages/Viewer.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Pdfviewer from "../Pdfviewer";
+import Link from "next/link";
 
 export default function Viewer() {
   return (
-    <div className='h-screen w-screen flex items-center justify-center'>Viewer</div>
-  )
+    <div className="min-h-screen w-screen mt-16 flex items-center justify-center overflow-hidden flex-col relative">
+      <Link
+        className="px-8 py-3 rounded-md bg-blue-500 fixed top-20 right-6 z-10 text-sm text-white"
+        href="https://gyannnnn.github.io/iitkirba.io/db/pyq/branch/it/2y/oop/oop_end_2024.pdf"
+      >
+        Download
+      </Link>
+
+      <Pdfviewer />
+    </div>
+  );
 }
