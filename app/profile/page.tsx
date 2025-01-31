c38730fac3e69ff5f62aed56985d9df7e76475a5
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-01-31 14:44:10 +0530

    fix; ui

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -29,7 +29,7 @@ export default async function Page() {
           <p>{user.email}</p>
           <h1 className="text-3xl">Your Saved PDFs</h1>
 
-          <div className="w-full min-h-screen flex flex-wrap items-start sm:justify-start justify-between gap-2 mt-5">
+          <div className="flex flex-wrap items-center sm:justify-start max-sm:justify-around  pt-10">
             {savedpdfs.length > 0 ? (
               savedpdfs.map((savedpdf) => (
                 <div className="relative" key={savedpdf.id}>
--- app/pyqs/loading.tsx
+++ app/pyqs/loading.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
 
-export default function loading() {
+export default function Loading() {
   return (
-    <div className='h-screen w-screen flex items-center justify-center bg-transparent text-white text-4xl'>
-      <Image src="/loader/ld1.gif" height={80} width={80} alt='loader' / >
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="relative w-12 aspect-square grid place-items-center">
+      <div className="absolute w-full aspect-square animate-spin border-4 border-transparent border-r-[#25b09b] rounded-full"></div>
+      <div className="absolute w-[calc(100%-4px)] aspect-square animate-[spin_2s_linear_infinite] border-4 border-transparent border-r-[#25b09b] rounded-full"></div>
+      <div className="absolute w-[calc(100%-16px)] aspect-square animate-[spin_3s_linear_infinite] border-4 border-transparent border-r-[#25b09b] rounded-full"></div>
     </div>
-  )
+    </div>
+  );
 }
