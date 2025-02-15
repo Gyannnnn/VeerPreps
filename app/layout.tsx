ca5349e26d20acfa18987de49b1af6c485951fef
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-02-15 09:58:25 +0530

    fix; default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default async function RootLayout({
           <SpeedInsights />
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="system"
             enableSystem={true}
             disableTransitionOnChange
           >
