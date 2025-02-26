2031dd88cf49e3600d1a21ab7ec8e8838877a220
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-02-26 10:57:13 +0530

    fix admin infite loader

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <div className="relative w-12 aspect-square grid place-items-center">
-      <div className="absolute w-full aspect-square animate-spin border-4 border-transparent border-r-[#2a4d9e] rounded-full"></div>
-      <div className="absolute w-[calc(100%-4px)] aspect-square animate-[spin_2s_linear_infinite] border-4 border-transparent border-r-[#2a4d9e] rounded-full"></div>
-      <div className="absolute w-[calc(100%-16px)] aspect-square animate-[spin_3s_linear_infinite] border-4 border-transparent border-r-[#2a4d9e] rounded-full"></div>
-    </div>
+    <div className="flex justify-center items-center h-screen bg-transparent">
+      <div className="flex space-x-2">
+        <div className="w-4 h-4 bg-blue-500 rounded-full animate-[bounce_0.5s_infinite_alternate] [animation-delay:-0.45s]"></div>
+        <div className="w-4 h-4 bg-blue-500 rounded-full animate-[bounce_0.5s_infinite_alternate] [animation-delay:-0.3s]"></div>
+        <div className="w-4 h-4 bg-blue-500 rounded-full animate-[bounce_0.5s_infinite_alternate] [animation-delay:-0.15s]"></div>
+        <div className="w-4 h-4 bg-blue-500 rounded-full animate-[bounce_0.5s_infinite_alternate]"></div>
+      </div>
     </div>
   );
 }
