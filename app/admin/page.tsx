37a112d41eb4d2f7f3bebfafe3705923477d6372
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-02-26 10:38:27 +0530

    fix padding bottom in admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ import Admincard from "../my_components/Admin";
 
 export default function page() {
   return (
-    <div className="min-h-screen w-screen mt-16 ">
+    <div className="min-h-screen w-screen mt-16 pb-6">
       <h1 className="text-3xl  pt-10 pb-4 pl-4 sm:pl-16 ">Admins</h1>
       <Admincard/>
     </div>
