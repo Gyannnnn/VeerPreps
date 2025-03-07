a628301e2f31e7aae61a2a9f28a0bf43668f9502
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-03-07 19:24:09 +0530

    fix; analytics

--- app/my_components/Analytics/GoogleAnalyticsClient.tsx
+++ app/my_components/Analytics/GoogleAnalyticsClient.tsx
@@ -3,7 +3,8 @@
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const GoogleAnalyticsClient = () => {
-  return <GoogleAnalytics trackPageViews />;
+  console.log("Google Analytics ID:", process.env.NEXT_PUBLIC_GA_ID);
+  return <GoogleAnalytics gaMeasurementId="G-TZ39FR5E5W" trackPageViews />;
 };
 
 export default GoogleAnalyticsClient;
