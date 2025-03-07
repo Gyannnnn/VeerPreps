ef2aa4ac8622306954b608089e900130d81984d0
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-03-07 19:33:53 +0530

    removed console.log

--- app/my_components/Analytics/GoogleAnalyticsClient.tsx
+++ app/my_components/Analytics/GoogleAnalyticsClient.tsx
@@ -3,7 +3,7 @@
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const GoogleAnalyticsClient = () => {
-  console.log("Google Analytics ID:", process.env.NEXT_PUBLIC_GA_ID);
+  
   return <GoogleAnalytics gaMeasurementId="G-TZ39FR5E5W" trackPageViews />;
 };
 
