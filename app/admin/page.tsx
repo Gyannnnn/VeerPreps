c375012ee35a5be42b6f20ddf347ed889905c1d5
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-03-13 11:42:11 +0530

    fix contact us ui

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import Admincard from "../my_components/Admin";
 
 export default function page() {
   return (
-    <div className="min-h-screen w-screen  items-center flex flex-col   justify-center">     
+    <div className="min-h-screen w-screen  items-center flex flex-col   justify-center max-sm:mt-16 max-sm:mb-2">   
 
       <Admincard />
     </div>
