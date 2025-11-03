4bcadcbadc9239bfd37887491e6c3f19065d7931
Gyan <hi.gyanaranjanpatra@gmail.com>
2025-11-03 07:42:30 +0530

    fix the ui on pdf viewwer

--- app/viewer/[...pdfid]/pdfviewer.tsx
+++ app/viewer/[...pdfid]/pdfviewer.tsx
@@ -296,7 +296,7 @@ console.log(links)
       <div className="w-full z-50 bg-white/80 dark:bg-zinc-900/80 backdrop-blur-md border-b border-gray-200 dark:border-zinc-800 lg:fixed lg:top-0 lg:left-0 lg:right-0">
         <div className="max-w-7xl mx-auto px-2 py-4 max-sm:mt-16 ">
           <div className="flex items-center justify-between gap-4 flex-wrap">
-            <div className="flex items-center gap-4 min-w-0 pl-0 sm:pl-1 bg-black">
+            <div className="flex items-center gap-4 min-w-0 pl-0 sm:pl-1">
               <div className="flex items-center gap-3 ml-[-4px] sm:ml-0 ">
                 <div className="p-2 bg-blue-100 dark:bg-blue-900/30 rounded-lg">
                   <FileText className="h-5 w-5 text-blue-600" />
